951b4dbf29d6af325350e02374727ff62fa1b87c
NickYeromin <homa.chups@gmail.com>
2020-01-14 01:37:01 +0200

    Голова,глаза,брови, осталось только рот.

--- lab3/Lab1.py
+++ lab3/Lab1.py
@@ -1,17 +1,18 @@
 from graph import *
 
-def Eye(x,y,radius): #Eye
+def Eye(x, y, radius): #Eye
 	brushColor("white")
-	circle(x,y,radius) #Ball
+	circle(x ,y, radius) #Ball
 	brushColor("black")
-	circle(x,y,radius/2) #Pupil
+	circle(x, y, radius/2) #Pupil
 
 brushColor("yellow")
-circle(200,200,150) #Head
-Eye(150,150,50) #Eye -left
-Eye(250,150,30)	#Eye -right
+circle(200, 200, 150) #Head
+Eye(150, 150, 50) #Eye -left
+Eye(250, 150, 30)	#Eye -right
 	
-	
+polygon([(100, 100), (200, 80),(100, 130)]) #Brow -left
+polygon([(210, 120), (300, 100),(210, 150)]) #Brow -right
 
 
 run()
